d881e1b9080e5d658de88e1bb5f472ab75bb8827
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-01-08 18:21:10 +0200

    my first commit

---
+++ blog/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Comment
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                        'id': 'nameInput'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                           "id": "exampleInputEmail1",
+                                                           "aria-describedby": "emailHelp"
+                                                           }))
+    to = forms.EmailField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                        "id": "emailTo"}))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={"class": "form-control",
+                                                                            "id": "comments",
+                                                                            "rows": "3"}))
+
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ('name', 'email', 'body')
+class CommentForm(forms.Form):
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                        'id': 'nameInput'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                           "id": "exampleInputEmail1",
+                                                           "aria-describedby": "emailHelp"
+                                                           }))
+    comment = forms.CharField(required=False, widget=forms.Textarea(attrs={"class": "form-control",
+                                                                            "id": "comments",
+                                                                            "rows": "3"}))
